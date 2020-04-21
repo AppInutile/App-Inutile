b8a333de9f12bdbb2bff4bae5df40c199fa7b9e7
AppInutile <poca.utilia@gmail.com>
2020-04-21 19:41:44 +0200

    init commit

---
+++ ContattamiForm.php
@@ -0,0 +1,14 @@
+<?php
+  if (isset($_POST['submit'])) {
+    $nome = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $oggetto = $_POST['oggetto'];
+    $messaggio = $_POST['messaggio'];
+
+    $mailTo = "poca.utilita@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Hai ricevuto un'e-mail da ".$nome.".\n\n".$messaggio;
+
+    mail($mailTo, $oggetto, $txt, $headers);
+    header("Location: Contattami.html?mailsend");
+  }
